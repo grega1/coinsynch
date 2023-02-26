a06dc54d5642183ef6ef9038a978d38265beb6ac
Adriano Grego <74581108+grega1@users.noreply.github.com>
2023-02-26 00:33:34 -0300

    :sparkles: feat: icons import and add color theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+type ITheme = Record<string, Record<string, string>>
+
+export const theme: ITheme = {
+  colors: {
+    primaryYellow: '#FBAB34',
+    secondaryYellow: '##FFF6E8',
+    secondary: '#8C8A97',
+    terciary: '#1BD171',
+    quaternary: '#EC3237',
+    white:'#FFFFFF',
+    black:'#000000',
+    textBase:'#5D6670'
+  },
+};
